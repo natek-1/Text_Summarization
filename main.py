a046134610857e639fd641e078d93f3b866ec913
natek-1 <nategabrielk@icloud.com>
2023-07-22 12:46:29 -0400

    done modular implmentation of data ingestion

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummarizer.logging import logger
 
-logger.info("Hello world custom log")
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"{'>'*10} {STAGE_NAME}  started {'<'*10}")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f"{'>'*10} {STAGE_NAME} completed {'<'*10} \n\n x{'='*20}x")
+except Exception as e:
+    logger.exception(e)
+    raise e
